cb39c49150c30054d12f4ba85bb048f46ffd8729
Shuxrat <shuxratturaev96@gmail.com>
2024-09-27 14:28:13 +0500

    color

---
+++ main.js
@@ -0,0 +1,153 @@
+let items = document.querySelectorAll('.item')
+let boxItem = document.querySelectorAll('.box_item')
+let body = document.body
+let boxItems1 = document.querySelector('.box__items1')
+    boxItems2 = document.querySelector('.box__items2')
+    boxItems3 = document.querySelector('.box__items3')
+    boxItems4 = document.querySelector('.box__items4')
+    boxItems5 = document.querySelector('.box__items5')
+    boxItems6 = document.querySelector('.box__items6')
+    
+  
+ items[0].addEventListener('click', () => {
+    body.style.background = 'red'
+    
+ })
+ items[1].addEventListener('click', () => {
+    body.style.background = 'yellow'
+ })
+ items[2].addEventListener('click', () => {
+    body.style.background = 'green'
+ })
+ items[3].addEventListener('click', () => {
+    body.style.background = 'blue'
+ })
+ items[4].addEventListener('click', () => {
+    body.style.background = 'white'
+ })
+ 
+ boxItem[0].addEventListener('click', () => {
+   boxItems1.style.background = 'red'
+ })
+ boxItem[0].addEventListener('click', () => {
+   boxItems2.style.background = 'red'
+ })
+ boxItem[0].addEventListener('click', () => {
+   boxItems3.style.background = 'red'
+ })
+ boxItem[0].addEventListener('click', () => {
+   boxItems4.style.background = 'red'
+ })
+ boxItem[0].addEventListener('click', () => {
+   boxItems5.style.background = 'red'
+ })
+ boxItem[0].addEventListener('click', () => {
+   boxItems6.style.background = 'red'
+ })
+ 
+ 
+ 
+ boxItem[1].addEventListener('click', () => {
+   boxItems1.style.background = 'yellow'
+ })
+ boxItem[1].addEventListener('click', () => {
+   boxItems2.style.background = 'yellow'
+ })
+ boxItem[1].addEventListener('click', () => {
+   boxItems3.style.background = 'yellow'
+ })
+ boxItem[1].addEventListener('click', () => {
+   boxItems4.style.background = 'yellow'
+ })
+ boxItem[1].addEventListener('click', () => {
+   boxItems5.style.background = 'yellow'
+ })
+ boxItem[1].addEventListener('click', () => {
+   boxItems6.style.background = 'yellow'
+ })
+ 
+ 
+ 
+ boxItem[2].addEventListener('click', () => {
+   boxItems1.style.background = 'green'
+ })
+ boxItem[2].addEventListener('click', () => {
+   boxItems2.style.background = 'green'
+ })
+ boxItem[2].addEventListener('click', () => {
+   boxItems3.style.background = 'green'
+ })
+ boxItem[2].addEventListener('click', () => {
+   boxItems4.style.background = 'green'
+ })
+ boxItem[2].addEventListener('click', () => {
+   boxItems5.style.background = 'green'
+ })
+ boxItem[2].addEventListener('click', () => {
+   boxItems6.style.background = 'green'
+ })
+ 
+ 
+ 
+ boxItem[3].addEventListener('click', () => {
+   boxItems1.style.background = 'blue'
+ })
+ boxItem[3].addEventListener('click', () => {
+   boxItems2.style.background = 'blue'
+ })
+ boxItem[3].addEventListener('click', () => {
+   boxItems3.style.background = 'blue'
+ })
+ boxItem[3].addEventListener('click', () => {
+   boxItems4.style.background = 'blue'
+ })
+ boxItem[3].addEventListener('click', () => {
+   boxItems5.style.background = 'blue'
+ })
+ boxItem[3].addEventListener('click', () => {
+   boxItems6.style.background = 'blue'
+ })
+ 
+ 
+ 
+ boxItem[4].addEventListener('click', () => {
+   boxItems1.style.background = 'white'
+ })
+ boxItem[4].addEventListener('click', () => {
+   boxItems2.style.background = 'white'
+ })
+ boxItem[4].addEventListener('click', () => {
+   boxItems3.style.background = 'white'
+ })
+ boxItem[4].addEventListener('click', () => {
+   boxItems4.style.background = 'white'
+ })
+ boxItem[4].addEventListener('click', () => {
+   boxItems5.style.background = 'white'
+ })
+ boxItem[4].addEventListener('click', () => {
+   boxItems6.style.background = 'white'
+ })
+   
+    
+ 
+ 
+
+   
+   
+   
+
+
+
+
+
+
+
+
+
+
+
+ 
+      
+    
+    
